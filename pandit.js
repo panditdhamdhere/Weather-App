d34e4fce5d0b6139d3d4eec9b2b8360cb12ad398
pandit <dhamdherepandit@gmail.com>
2022-12-08 19:45:33 +0530

    written javascript

--- pandit.js
+++ pandit.js
@@ -1,3 +1,5 @@
+// Rapid APIs
+
 const options = {
 	method: 'GET',
 	headers: {
@@ -6,8 +8,32 @@ const options = {
 	}
 };
 
+
+const getWeather = (city)=> {
+	cityName.innerHTML = city;
 fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Pune', options)
 	.then(response => response.json())
 	.then(response => {
-        console.log(response)})
+		console.log(response)
+
+		cloud_pct.innerHTML = response.cloud_pct
+		temp.innerHTML = response.temp
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		min_temp.innerHTML = response.min_temp
+		max_temp.innerHTML = response.max_temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_degrees.innerHTML = response.wind_degrees
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunse
+
+
+	})
 	.catch(err => console.error(err));
+}
+
+submit.addEventListener("click", (e) => {
+	getWeather(city.value)
+})
+
+getWeather("Delhi")
